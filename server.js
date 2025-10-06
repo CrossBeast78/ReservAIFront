b2c746f1770155058eafcfb5307ecd6a525feb16
Rafael Velazco <rafitavelazco@hotmail.com>
2025-10-06 14:16:28 -0600

    cambio en el server

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 const PORT = process.argv[2] || 3000
 const router = require('./public/Scripts/router');
 
+router.use((req, res, next) => {
+    // Si la ruta viene con /api, la removemos para procesamiento interno
+    if (req.path.startsWith('/view')) {
+        req.url = req.url.replace('/view', '');
+        req.originalUrl = req.originalUrl.replace('/view', '');
+    }
+    next();
+});
+
 // Ruta para servir el HTML
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));

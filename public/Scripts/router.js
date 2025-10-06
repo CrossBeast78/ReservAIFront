1fa2861a9faf7d91f951bee44c5244806b794734
Rafael Velazco <rafitavelazco@hotmail.com>
2025-10-06 14:25:23 -0600

    cambio router

--- public/Scripts/router.js
+++ public/Scripts/router.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+router.use((req, res, next) => {
+    // Si la ruta viene con /api, la removemos para procesamiento interno
+    if (req.path.startsWith('/view')) {
+        req.url = req.url.replace('/view', '');
+        req.originalUrl = req.originalUrl.replace('/view', '');
+    }
+    next();
+});
+
+
 // Definir rutas
 router.get('/', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/login.html")));
 router.get('/login', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/login.html")));
@@ -10,5 +20,7 @@ router.get('/register', (req, res) => res.sendFile(path.resolve(__dirname + "/
 router.get('/QR', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/QR.html")));
 router.get('/verify_email', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/verify_email.html")));
 router.get('/twofa', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/twofa.html")));
+
+router.get('')
 
 module.exports = router;
